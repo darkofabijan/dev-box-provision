b3cae7cd54deafd18e804bafa4aba157700f4b55
Darko Fabijan <darko@renderedtext.com>
2015-07-15 22:45:35 +0200

    Initial commit

---
+++ user_provision.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -eux
+
+rm -rf dot .vim .vimrc .oh-my-zsh
+
+git clone git@github.com:darkofabijan/dot.git dot
+cd dot
+./install.sh
+cd
+
+git clone git@github.com:darkofabijan/vim-web-dev.git .vim
+git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+ln -s ~/.vim/vimrc ~/.vimrc
+
+sudo apt-get install -y zsh
+sudo chsh -s $(which zsh) vagrant
+
+sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)" || echo "ok"
